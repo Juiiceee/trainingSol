d9d045fab30e8cebc3a828c7d1082acb5824c720
Juiiceee <lbehr@student.42.fr>
2025-08-21 17:07:23 +0200

    compteur good

--- compteur/programs/compteur/src/lib.rs
+++ compteur/programs/compteur/src/lib.rs
@@ -7,10 +7,49 @@ pub mod compteur {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+		ctx.accounts.compteur.count = 0;
+		Ok(())
+    }
+
+	pub fn increment(ctx: Context<Increment>) -> Result<()> {
+		ctx.accounts.compteur.count = ctx.accounts.compteur.count.checked_add(1).unwrap();
+		Ok(())
+    }
+
+	pub fn decrement(ctx: Context<Decrement>) -> Result<()> {
+		ctx.accounts.compteur.count = ctx.accounts.compteur.count.checked_sub(1).unwrap();
+		Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+	#[account(init, payer = signer, space = 8 + 8)]
+	pub compteur: Account<'info, Compteur>,
+	#[account(mut)]
+	pub signer: Signer<'info>,
+	pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+	#[account(mut)]
+	pub compteur: Account<'info, Compteur>,
+	#[account(mut)]
+	pub signer: Signer<'info>,
+	pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Decrement<'info> {
+	#[account(mut)]
+	pub compteur: Account<'info, Compteur>,
+	#[account(mut)]
+	pub signer: Signer<'info>,
+	pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Compteur {
+	pub count: u64,
+}
--- compteur/tests/compteur.ts
+++ compteur/tests/compteur.ts
@@ -1,16 +1,48 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Compteur } from "../target/types/compteur";
+import { expect } from "chai";
+import BN from "bn.js";
 
 describe("compteur", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.compteur as Program<Compteur>;
+	const provider = anchor.getProvider();
+	const compteurAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods.initialize().accounts({
+		compteur: compteurAccount.publicKey,
+		signer: provider.wallet.publicKey,
+	})
+	.signers([compteurAccount])
+	.rpc();
+	const compteur = await program.account.compteur.fetch(compteurAccount.publicKey);
+	expect(compteur.count.toNumber()).to.equal(0);
+	console.log("Your transaction signature", tx);
   });
+
+  it("Is incremented!", async () => {
+	const tx = await program.methods.increment().accounts({
+		compteur: compteurAccount.publicKey,
+		signer: provider.wallet.publicKey,
+	}).rpc();
+	const compteur = await program.account.compteur.fetch(compteurAccount.publicKey);
+	expect(compteur.count.toNumber()).to.equal(1);
+	console.log("Your transaction signature", tx);
+  });
+
+  it("Is decremented!", async () => {
+	const tx = await program.methods.decrement().accounts({
+		compteur: compteurAccount.publicKey,
+		signer: provider.wallet.publicKey,
+	}).rpc();
+	const compteur = await program.account.compteur.fetch(compteurAccount.publicKey);
+	expect(compteur.count.toNumber()).to.equal(0);
+	console.log("Your transaction signature", tx);
+  });
+
 });
