60e0d7e6efdacb3d04cb7474394bfb5410ea5ef3
Juiiceee <lbehr@student.42.fr>
2025-08-22 11:40:54 +0200

    good init + fmt

--- vault/programs/vault/src/lib.rs
+++ vault/programs/vault/src/lib.rs
@@ -1,16 +1,51 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{ transfer, Transfer };
 
 declare_id!("62dPU3FNV5a78JSv7dkcrjDRKackq1KhAJrFFYNEZAJr");
 
 #[program]
 pub mod vault {
-    use super::*;
+	use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+	pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+		ctx.accounts.vault.owner = ctx.accounts.signer.key();
+		ctx.accounts.vault.amount = 0;
+		ctx.accounts.vault.bump = ctx.bumps.vault;
+		Ok(())
+	}
+
+	pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+		ctx.accounts.vault.amount = amount.checked_add(ctx.accounts.vault.amount).unwrap();
+		let cpi_context = CpiContext::new(ctx.accounts.system_program.to_account_info(), Transfer {
+			from: ctx.accounts.signer.to_account_info(),
+			to: ctx.accounts.vault.to_account_info(),
+		});
+		transfer(cpi_context, amount)?;
+		Ok(())
+	}
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+	#[account(init, payer = signer, space = 8 + 32 + 8 + 1, seeds = [b"vault"], bump)]
+	pub vault: Account<'info, Vault>,
+	#[account(mut)]
+	pub signer: Signer<'info>,
+	pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+	#[account(mut)]
+	pub vault: Account<'info, Vault>,
+	#[account(mut)]
+	pub signer: Signer<'info>,
+	pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Vault {
+	pub owner: Pubkey,
+	pub amount: u64,
+	pub bump: u8,
+}
--- vault/tests/vault.ts
+++ vault/tests/vault.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import { expect } from "chai";
+import BN from "bn.js";
 
 describe("vault", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	// Configure the client to use the local cluster.
+	anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.vault as Program<Vault>;
+	const program = anchor.workspace.vault as Program<Vault>;
+	const provider = anchor.getProvider();
+	const [vaultAccount] = anchor.web3.PublicKey.findProgramAddressSync(
+		[Buffer.from("vault")],
+		program.programId
+	);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	it("Is initialized!", async () => {
+		// Add your test here.
+		const tx = await program.methods
+			.initialize()
+			.accounts({
+				vault: vaultAccount,
+				signer: provider.wallet.publicKey,
+				systemProgram: anchor.web3.SystemProgram.programId,
+			})
+			.rpc();
+		const vault = await program.account.vault.fetch(vaultAccount);
+		expect(vault.amount.toNumber()).to.equal(0);
+		console.log("amount: ", vault.amount.toString());
+		expect(vault.owner.toBase58()).to.equal(provider.wallet.publicKey.toBase58());
+		console.log("owner: ", vault.owner.toBase58());
+		console.log("Your transaction signature", tx);
+	});
 });
